77e7f59357fd08d7f0b1fde691494d408049c0c5
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2022-01-19 15:18:06 +0000

    Update Discord_Cache_Viewer.bat

--- Discord_Cache_Viewer.bat
+++ Discord_Cache_Viewer.bat
@@ -322,8 +322,8 @@ if defined Discord_Running (
 echo:
 echo  ■ [INFORMATION] Processing the 'data_1' cached URLs ...
 echo  ├ [INFORMATION] Checking your internet connection ...
->nul curl -fkLs https://1.1.1.1/ || (
-    >nul curl -fkLs https://8.8.8.8/ || (
+>nul curl -fIkLs https://1.1.1.1/ || (
+    >nul curl -fIkLs https://8.8.8.8/ || (
         echo  └ [WARNING    ] No internet connection detected. Downloading the 'data_1' cached URLs aborted.
         goto :FINISHED
     )
